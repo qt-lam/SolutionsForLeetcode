b1599500606782aad652a7fc944ac26696327def
QiuJunYin <qiujunyin@foxmail.com>
2021-05-23 14:46:35 +0800

    1707 暴力尝试

--- 1707与数组中元素的最大异或值.cpp
+++ 1707与数组中元素的最大异或值.cpp
@@ -12,8 +12,45 @@
 #include <queue>
 using namespace std;
 
+//±©¡¶∑®≥¢ ‘
+class Solution {
+public:
+    vector<int> maximizeXor(vector<int>& nums, vector<vector<int>>& queries) {
+        vector<int> answers;
+        if(queries.empty()||nums.empty()){
+            return answers;
+        }
 
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<queries.size();i++){
+            int first=queries[i][0];
+            int second=queries[i][1];
+            int Max=-1;
+            int j=0;
+
+            for(;j<nums.size();j++){
+                if(nums[j]>second){
+                    answers.push_back(Max); 
+                    break;
+                }
+                Max=max(Max,nums[j]^first);
+            }
+            if(j==nums.size()){
+                answers.push_back(Max);
+            }
+        }
+
+        return answers;
+    }
+};
+
+
+//≤‚ ‘
 int main()
 {
-    
+    vector<int> nums{0,1,2,3,4};
+    vector<vector<int>>queries{{3,1},{1,3},{5,6}};
+    Solution a;
+    a.maximizeXor(nums,queries);
+    return 0;
 }
